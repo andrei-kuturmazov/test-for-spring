ca3c7aaa118edcebb23ba7c6926d538fc6a54a7a
andrei-kuturmazov <andrei.kuturmazov@gmail.com>
2020-02-20 16:32:31 +0300

    properties adding

--- src/main/java/andrei/spring/app/MusicPlayer.java
+++ src/main/java/andrei/spring/app/MusicPlayer.java
@@ -1,14 +1,18 @@
 package andrei.spring.app;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
+@Getter
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
 
     public void playMusic() {
         System.out.println("Playing " + music.getSong());
